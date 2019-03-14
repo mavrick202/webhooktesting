b5364f19c75de1e427597c19faf27be06e737a07
Mavrick-DevOps <mavrick202@gmail.com>
2019-03-14 08:57:21 +0000

    Updatde rm nginx

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 VERSION=$(date +%H-%M-%S)
 docker build -t sreeharshav/testingbuild:${VERSION} .
 docker push sreeharshav/testingbuild:${VERSION}
+docker -H tcp://10.1.1.109:2375 stop nginx
 docker -H tcp://10.1.1.109:2375 run --rm -dit -p 8000:80 --name nginx --hostname nginx sreeharshav/testingbuild:${VERSION}
